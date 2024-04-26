0ec2aecbd230c17a2841a1307e3ed0f1f6046a17
Frank Wang <121549293+Art-Pig@users.noreply.github.com>
2024-04-26 11:19:54 +0800

    Create struct_in_c.c

---
+++ struct_in_c.c
@@ -0,0 +1,37 @@
+/*
+
+
+*C语言结构体
+
+*C++也有
+
+*以图书目录举例
+
+
+
+*/
+
+
+#include <stdio.h>
+
+struct class {
+
+  int numbers;
+
+  string name;
+
+}libary;  //构造一个名为libary的数据类型
+
+int mian(void){
+
+  libary little;  //声明变量
+
+  little.numbers = 2;  //为其赋值
+
+  little.name = "little pricne";
+
+  println("The \"little\" Numbers is %d and the name is %s. \n",numbers,name);  //打印出它的数据
+
+  return 0;
+
+}
